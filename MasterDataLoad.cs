19d9a159d59b87e0503dbd0452edc5b809a5ff61
Vicky <69034338+sjin11125@users.noreply.github.com>
2023-05-01 04:05:35 +0900

    Create MasterDataLoad.cs

---
+++ MasterDataLoad.cs
@@ -0,0 +1,12 @@
+﻿namespace Com2usServerCampus
+{
+    public class MasterDataLoad
+    {
+        List<ItemData> itemDatas = new List<ItemData>();
+        List<ItemAttribute> itemAttributes = new List<ItemAttribute>();
+        List<AttendanceReward>   attendanceRewards = new List<AttendanceReward>();
+        List<InAppProduct> inAppProducts = new List<InAppProduct>();
+        List<StageItem> stageItems = new List<StageItem>();
+        List<StageNPC> stageNPCs = new List<StageNPC>();
+    } 
+}
